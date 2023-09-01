33be7744f2ffc8b635319d26dbaf7b3441e60ae3
RodrigoF32 <rodrigofreitas3223@outlook.com>
2023-09-01 16:38:16 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int colimns;
+	private Piece[][] pieces;
+	
+	
+	public Board(int rows, int colimns) {
+		this.rows = rows;
+		this.colimns = colimns;
+	}
+
+	
+	
+}
